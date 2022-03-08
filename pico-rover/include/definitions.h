81928fa0524dd69072523f80d123edf7515b1473
Wesley Fletcher <wesfletch@gmail.com>
2022-03-07 19:54:13 -0500

    reorg, adds header guards

--- pico-rover/include/definitions.h
+++ pico-rover/include/definitions.h
@@ -1,28 +1,30 @@
-// general includes
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// hardware includes
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
-#include "hardware/irq.h"
-#include "hardware/i2c.h"
-#include "hardware/pwm.h"
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
 
 // define UART connection for GPS
-#define UART_ID_GPS     uart0
-#define BAUD_RATE_GPS   9600
-#define DATA_BITS_GPS   8
-#define STOP_BITS_GPS   1
-#define PARITY_GPS      UART_PARITY_NONE
+#define UART_ID_GPS         uart0
+#define BAUD_RATE_GPS       9600
+#define DATA_BITS_GPS       8
+#define STOP_BITS_GPS       1
+#define PARITY_GPS          UART_PARITY_NONE
+#define UART_TX_PIN_GPS     0
+#define UART_RX_PIN_GPS     1
+
+
+// define UART connection for GPS
+#define UART_ID_LORA        uart1
+#define BAUD_RATE_LORA      115200
+#define DATA_BITS_LORA      8
+#define STOP_BITS_LORA      1
+#define PARITY_LORA         UART_PARITY_NONE
+#define UART_TX_PIN_LORA    4
+#define UART_RX_PIN_LORA    5
+
 
 // We are using pins 0 and 1, but see the GPIO function select table in the
 // datasheet for information on which other pins can be used.
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
-#define PWM_1_PIN   16
-#define PWM_2_PIN   17
+#define PWM_1_PIN           16
+#define PWM_2_PIN           17
 
 // for string parsing
 #define ENDSTDIN    255 // NULL
@@ -30,14 +32,13 @@
 #define CR          13  // CARRIAGE RETURN
 
 // message definitions
-static const char * MSG_MOTORS = "$MOTORS\0";     // command for motor controller
-static const char * MSG_TX     = "$TX\0";         // transmit a string on the LoRa
-static const char * MSG_CMD    = "$CMD\0";        // generic command, passed to SBC through serial
-static const char * MSG_REQ    = "$REQ\0";        // a request for data update, new rate, etc...
-static const char * MSG_ACK    = "$ACK\0";        // an acknowledgement that a message was received
+static const char * MSG_MOTORS = "$MTR\0";      // command for motor controller
+static const char * MSG_TX     = "$TXR\0";      // transmit a string on the LoRa
+static const char * MSG_CMD    = "$CMD\0";      // generic command, passed to SBC through serial
+static const char * MSG_REQ    = "$REQ\0";      // a request for data update, new rate, etc...
+static const char * MSG_ACK    = "$ACK\0";      // an acknowledgement that a message was received
+// message buffer sizes
+#define NMEA_SIZE   83
+#define LORA_SIZE   260
 
-// for later
-typedef struct MESSAGE {
-    uint32_t timestamp;
-    char *msg;
-} MESSAGE;
+#endif
---
+++ pico-rover/include/main.h
@@ -0,0 +1,43 @@
+/**
+ * @file main.h
+ * @author Wesley Fletcher (wkfletcher@knights.ucf.com)
+ * @brief Function prototypes for the "main" program to be loaded onto the Pico
+ * @version 0.1
+ * @date 2022-03-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// general includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// hardware includes
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
+
+#include "../include/definitions.h"
+#include "../include/comms.h"
+
+void on_UART_GPS_rx();
+void on_UART_LORA_rx();
+int configure_UART(uart_inst_t *UART_ID, uint BAUDRATE, uint TX_PIN, uint RX_PIN, uint DATA_BITS, uint STOP_BITS, uint PARITY, irq_handler_t IRQ_FUN);
+
+int configure_PWM();
+void setPWM();
+
+int handle_input(char *in);
+
+#endif
+
+
+
