2bde7b0b931aaebd92dea7bb81b6a631f4cdb8a4
Wes Fletcher <wfletcher@geisel-software.com>
2025-03-28 16:08:10 -0400

    kill worthless line

--- include/rookie_pico/Commands.hpp
+++ include/rookie_pico/Commands.hpp
@@ -7,7 +7,6 @@
 #include <string>
 #include <sstream>
 
-// #include <rookie_pico/Definitions.hpp>
 #include <pico_interface/PicoInterface.hpp>
 
 using CommandCallbacks = std::map<std::string, std::function<bool(std::string)>>;
